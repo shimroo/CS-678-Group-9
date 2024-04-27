e30951b331a76544a9dd52ff511fbf1cf712d0f9
shimroo <arafayb@gmail.com>
2024-04-28 00:51:00 +0500

    updated user model

--- web_form/backend/models/user.js
+++ web_form/backend/models/user.js
@@ -4,6 +4,11 @@ const userSchema = new mongoose.Schema({
     name : { type: String, required: true },
     age : { type: Number, required: true },
     gender : { type: String, required: true },
+    status : { type: String, required: true },
+    stance1 : { type: String, required: true },
+    stance2 : { type: String, required: true },
+    rate1 : { type: String },
+    rate2 : { type: String}
 });
 
 export const User = mongoose.model('User', userSchema);
